b3723ef607f8e3127e38c6394a0c23c90dd4b9b6
moisesabraao <moisesdirt_bmx@hotmail.com>
2018-05-10 10:54:24 -0300

    iniciando essa parada

---
+++ sorteio.py
@@ -0,0 +1,11 @@
+#coding:utf8
+import random
+
+def sorteio():
+    names = ['name1', 'name2','...']
+
+    #kkkkkkkk
+    for i in range(1,100000):
+        print i
+
+    print ("Parabéns " + random.choice(names))
